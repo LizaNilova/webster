a80097b5850839275f1d0ce2dfa4fe1bf2c21451
Herman <ushkalo.herman@gmail.com>
2023-04-25 18:09:20 +0300

    Create user-roles.model.ts

---
+++ server/src/roles/user-roles.model.ts
@@ -0,0 +1,20 @@
+import {
+  Column,
+  DataType,
+  ForeignKey,
+  Model,
+  Table,
+} from 'sequelize-typescript';
+import { User } from '../users/users.model';
+import { Role } from './roles.model';
+
+@Table({ tableName: 'user_roles', createdAt: false, updatedAt: false })
+export class UserRoles extends Model<UserRoles> {
+  @ForeignKey(() => Role)
+  @Column({ type: DataType.INTEGER })
+  roleId: number;
+
+  @ForeignKey(() => User)
+  @Column({ type: DataType.INTEGER })
+  userId: number;
+}
