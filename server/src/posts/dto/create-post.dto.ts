b8ac35dd7065d18c31c0e32f89091062e2ea2735
Herman <ushkalo.herman@gmail.com>
2023-04-25 18:08:46 +0300

    Create create-post.dto.ts

---
+++ server/src/posts/dto/create-post.dto.ts
@@ -0,0 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreatePostDto {
+  readonly title: string;
+  readonly content: string;
+  readonly image: string;
+  readonly userId: number;
+}
