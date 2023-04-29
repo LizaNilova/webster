6b6b9882b2156af78a1cc688f23698a92b1a8011
Herman <ushkalo.herman@gmail.com>
2023-04-29 18:45:31 +0300

    add create cookie in login and create endpoint refresh

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,22 +1,38 @@
-import { Body, Controller, HttpCode, Injectable, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Post, Req, Res } from '@nestjs/common';
 import { ApiParam, ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { AuthService } from './auth.service';
+import { Request, Response } from 'express';
 
 @ApiTags('Authentication')
 @Controller('auth')
 export class AuthController {
-  constructor(private authervice: AuthService) {}
+  constructor(private authervice: AuthService) { }
 
   @Post('/login')
   @HttpCode(200)
-  login(@Body() userDto: CreateUserDto) {
-    return this.authervice.login(userDto);
+  async login(@Body() userDto: CreateUserDto, @Res({ passthrough: true }) response: Response) {
+    const tokens = await this.authervice.login(userDto);
+    response.cookie('refreshToken', tokens.refreshToken, {
+      httpOnly: true,
+      expires: new Date(Date.now() + 1 * 24 * 60 * 1000),
+    });
+    return { accessToken: tokens.accessToken };
   }
 
   @Post('/registration')
   registration(@Body() userDto: CreateUserDto) {
-    console.log(userDto)
     return this.authervice.registration(userDto);
+  }
+
+  @Get('/refresh')
+  async refresh(@Req() request: Request, @Res({ passthrough: true }) response: Response) {
+    const tokens = await this.authervice.refresh(request.cookies['refreshToken']);
+    response.clearCookie('refreshToken');
+    response.cookie('refreshToken', tokens.refreshToken, {
+      httpOnly: true,
+      expires: new Date(Date.now() + 1 * 24 * 60 * 1000),
+    });
+    return { accessToken: tokens.accessToken };
   }
 }
