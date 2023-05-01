5c7f49d3b3c51732c7c73f0310191e96e5802b91
Maksym Rekalo <maksim.monor@gmail.com>
2023-05-01 19:14:29 +0200

    trash

--- webster_front/src/App.jsx
+++ webster_front/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState, useEffect } from 'react';
 import './App.css'
+import {fabric} from 'fabric'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+export default function App() {
+  const [canvas, setCanvas] = useState('');
+  useEffect(() => {
+    setCanvas(initCanvas());
+  }, []);
+  const initCanvas = () => (
+    new fabric.Canvas('canvas', {
+      height: 800,
+      width: 800,
+      backgroundColor: 'pink'
+    })
   )
+  return(
+    <div>
+      <h1>Fabric.js on React - fabric.Canvas('...')</h1>
+      <canvas id="canvas" />
+    </div>
+  );
 }
 
-export default App
