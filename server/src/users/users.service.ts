7f444db96b40c94710cb5fc52942f07637ae4ca5
Herman <ushkalo.herman@gmail.com>
2023-05-04 18:40:19 +0300

    add confirm user account

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,11 +1,13 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { User } from './users.model';
+import { User } from './models/users.model';
 import { InjectModel } from '@nestjs/sequelize';
 import { CreateUserDto } from './dto/create-user.dto';
 import { RolesService } from '../roles/roles.service';
 import { AddRoleDto } from './dto/add-role.dto';
 import { BanUserDto } from './dto/ban-user.dto';
-import { UserBanned } from './user-banned.model';
+import { UserBanned } from './models/user-banned.model';
+import { UserEvents } from './models/user-event.model';
+import { UserEventDto } from './dto/user-event.dto';
 
 @Injectable()
 export class UsersService {
@@ -13,7 +15,8 @@ export class UsersService {
     @InjectModel(User) private userRepository: typeof User,
     private roleService: RolesService,
     @InjectModel(UserBanned) private userBennedRepository: typeof UserBanned,
-  ) {}
+    @InjectModel(UserEvents) private userEventRepository: typeof UserEvents,
+  ) { }
 
   async createUser(dto: CreateUserDto) {
     const user = await this.userRepository.create(dto);
@@ -23,13 +26,18 @@ export class UsersService {
     return user;
   }
 
+  async createEvent(data: UserEventDto) {
+    const event = await this.userEventRepository.create(data);
+    return event;
+  }
+
   async getAllUsers() {
     const users = await this.userRepository.findAll({ include: { all: true } });
     return users;
   }
 
   async getUserById(id: number) {
-    const user = await this.userRepository.findOne({where: {id}, include: { all: true } });
+    const user = await this.userRepository.findOne({ where: { id }, include: { all: true } });
     return {
       id: user.id,
       login: user.login,
@@ -62,6 +70,20 @@ export class UsersService {
       return dto;
     }
     throw new HttpException('Undefined role or user', HttpStatus.NOT_FOUND);
+  }
+
+  async confirm(eventId: string, code: string): Promise<User> {
+    const event = await this.userEventRepository.findByPk(eventId);
+    if (!event) {
+      throw new HttpException({ message: 'Session not found' }, HttpStatus.NOT_FOUND);
+    }
+    if (code !== event.event_content) {
+      throw new HttpException({ message: 'Code do not match' }, HttpStatus.BAD_REQUEST);
+    }
+    await this.userEventRepository.destroy({ where: { id: eventId } });
+    const user = await this.userRepository.findByPk(event.userId, { include: { all: true } });
+    await user.update({ is_active: true });
+    return user;
   }
 
   async ban(dto: BanUserDto) {
