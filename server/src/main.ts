fcea0fe13baaa1fe4b10118811be1f0322415145
Herman <ushkalo.herman@gmail.com>
2023-05-04 18:46:49 +0300

    Update main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
 
-async function start() {
+async function bootstrap() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
   app.enableCors({
@@ -20,8 +20,7 @@ async function start() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/api/docs', app, document);
 
-
   await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
 }
 
-start();
+bootstrap();
