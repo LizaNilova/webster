77d821bbbfc349788d5fd31af8f4b71565845987
Herman <ushkalo.herman@gmail.com>
2023-05-05 22:59:08 +0300

    Update main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,9 +7,9 @@ async function bootstrap() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.URL_CLIENT,
-    credentials: true,
-  });
+      origin: process.env.URL_CLIENT,
+      credentials: true,
+    });
   app.use(cookieParser());
   const config = new DocumentBuilder()
     .setTitle('Webster')
