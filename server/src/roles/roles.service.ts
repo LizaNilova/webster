564a3b0af87432d8e00f6dc3f6ca609957634cb4
Herman <ushkalo.herman@gmail.com>
2023-05-07 19:34:28 +0300

    change condition

--- server/src/roles/roles.service.ts
+++ server/src/roles/roles.service.ts
@@ -8,6 +8,10 @@ export class RolesService {
   constructor(@InjectModel(Role) private roleRepository: typeof Role) {}
 
   async createRole(dto: CreateRoleDto) {
+    const isTruth = await this.isExistRole(dto.value);
+    if (isTruth) {
+      throw new BadRequestException('Role is exist');
+    }
     const role = await this.roleRepository.create(dto);
     return role;
   }
