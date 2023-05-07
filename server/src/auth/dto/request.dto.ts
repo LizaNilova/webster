db2fbea6d08734671010f4158b69003c87591e0f
Herman <ushkalo.herman@gmail.com>
2023-05-07 19:31:27 +0300

    add validation and field role

--- server/src/auth/dto/request.dto.ts
+++ server/src/auth/dto/request.dto.ts
@@ -4,6 +4,7 @@ import { TokenPayloadDto } from './token-payload.dto';
 export interface RequestDto extends Request {
     user: {
       id: number;
+      role: string;
     };
     body: {
       code: string;
