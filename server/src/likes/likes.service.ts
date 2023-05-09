9927f2f6e75beaad4e496c8707787f4eee51dee1
Herman <ushkalo.herman@gmail.com>
2023-05-09 17:28:15 +0300

    change logic response data and add exceptions

--- server/src/likes/likes.service.ts
+++ server/src/likes/likes.service.ts
@@ -1,22 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { Like } from './likes.model';
+import { PostsService } from 'src/posts/posts.service';
 
+interface ResultLikeDto {
+  message: string;
+  status: number;
+}
 @Injectable()
 export class LikesService {
-  constructor(@InjectModel(Like) private likesRepository: typeof Like) { }
+  constructor(@InjectModel(Like) private likesRepository: typeof Like,
+    private postService: PostsService) { }
 
-  async update(postId: number, userId: number) {
-    try {
-       const is_like = await this.likesRepository.findOne({ where: { userId, postId } })
-    if (is_like) { 
-      await is_like.destroy(); 
-      return 'remove like' 
+  async likePost(postId: number, userId: number): Promise<ResultLikeDto> {
+    const post = await this.postService.getById(postId);
+    if (!post) {
+      throw new NotFoundException('Post undefiend');
+    }
+    const is_like = await this.likesRepository.findOne({ where: { userId, postId } })
+
+    if (is_like) {
+      await is_like.destroy();
+      return {
+        message: 'remove like',
+        status: 200
+      }
     }
     await this.likesRepository.create({ userId, postId });
-    return 'like';
-    } catch (error) {
-      console.log(error)
+    return {
+      message: 'like',
+      status: 201
     }
+
   }
 }
