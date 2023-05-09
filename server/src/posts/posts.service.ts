559a64a541b1ede1fcf205a8a8eab23cd7fa6745
Herman <ushkalo.herman@gmail.com>
2023-05-09 03:03:44 +0300

    Update posts.service.ts

--- server/src/posts/posts.service.ts
+++ server/src/posts/posts.service.ts
@@ -1,100 +1,124 @@
-import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus, BadRequestException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { Post } from './posts.model';
-import { Category } from 'src/categories/categories.model';
+import { Category } from '../categories/categories.model';
 import { CreatePostDto } from './dto/create-post.dto';
 import { AddCategoryDto } from './dto/add-category.dto'
 import { FilesService } from '../files/files.service';
 import { CategoriesService } from '../categories/categories.service';
 import { Op, FindOptions } from 'sequelize';
+import { User } from '../users/models/users.model';
+import { PostCategory } from '../categories/post-category.model';
 
 @Injectable()
 export class PostsService {
 
-    constructor(@InjectModel(Post) private postsRepository: typeof Post, private categoriesService: CategoriesService,
-        private filesService: FilesService, @InjectModel(Category) private categoryRepository: typeof Category) { }
+  constructor(@InjectModel(Post) private postsRepository: typeof Post, private categoriesService: CategoriesService,
+    private filesService: FilesService, @InjectModel(Category) private categoryRepository: typeof Category) { }
 
-    async create(dto: CreatePostDto, image: Express.Multer.File) {
-        try {
-        const filename = await this.filesService.createFile(image);
+  async create(dto: CreatePostDto) {
+    const filename = await this.filesService.createFile(dto.image);
 
-        const existPost = await this.postsRepository.findOne({ where: { title: dto.title } });
-        if (existPost) {
-            throw new HttpException('Post already exists', HttpStatus.BAD_REQUEST);
-        }
-
-        const categories = await this.categoryRepository.findAll({ where: { value: { [Op.in]: dto.value } } });
-        if (categories.length !== dto.value.length) {
-            throw new HttpException(`One or more categories not found`, HttpStatus.NOT_FOUND);
-        }
-
-        const post = await this.postsRepository.create({ ...dto, image: filename }, { include: { all: true } });
-        await post.$add('categories', [...categories.map(category => category.id)]);
-        await post.reload();
-        return { post };
-        } catch (error) {
-            console.log(error)
-        }
+    const existPost = await this.postsRepository.findOne({ where: { title: dto.title } });
+    if (existPost) {
+      throw new HttpException('Post already exists', HttpStatus.BAD_REQUEST);
     }
 
-    async getById(id: number) {
-        const post = await this.postsRepository.findByPk(id, { include: { all: true } });
-        return post;
+    const categories = await this.categoryRepository.findAll({ where: { value: { [Op.in]: dto.value } } });
+    if (categories.length !== dto.value.length) {
+      throw new HttpException(`One or more categories not found`, HttpStatus.NOT_FOUND);
     }
 
-    async getAll(sort, filter, search) {
-        filter = filter ? JSON.parse(filter) : [];
+    const post = await this.postsRepository.create({ ...dto, image: filename }, { include: [{ all: true }, { model: User, attributes: ['id', 'login', 'email'] }] });
+    await post.$add('categories', [...categories.map(category => category.id)]);
+    await post.reload();
+    return post;
+  }
 
-        const filterOptions: FindOptions<Post> = {
-            include: (filter.length > 0) ? [{ model: Category, where: {} }, { all: true }] : { all: true },
-            order: (sort === 'byCategories') ? [[{ model: Category, as: 'categories' }, 'value', 'ASC']] : [['createdAt', 'DESC']],
-            where: {}
-        }
+  async getById(id: number) {
+    const post = await this.postsRepository.findByPk(id, {
+      include: [
+        { all: true },
+        { model: Category, where: {}, attributes: ['id', 'value', 'description'] },
+        { model: User, attributes: ['id', 'login', 'email'] }
+      ],
+    });
+    return post;
+  }
 
-           console.log(filterOptions)
+  async getAll(sort, filter, search) {
+    filter = filter ? JSON.parse(filter) : [];
 
-        if (search) {
-            filterOptions.where = { title: { [Op.iLike]: `%${search}%` } };
-        }
-
-        if (filter.length > 0) {
-            filterOptions.include[0].where = { value: { [Op.in]: filter } };
-        }
-
-        return await this.postsRepository.findAll(filterOptions);
+    const filterOptions: FindOptions<Post> = {
+      include: [
+        { all: true },
+        { model: Category, where: {}, attributes: ['id', 'value', 'description'] },
+        { model: User, attributes: ['id', 'login', 'email'] }
+      ],
+      order: (sort === 'byCategories') ? [[{ model: Category, as: 'categories' }, 'value', 'ASC']] : [['createdAt', 'DESC']],
+      where: {}
     }
 
-    async editPost(dto: CreatePostDto, userId: number, id: number, image?: any) {
-        let post = await this.postsRepository.findOne({ where: { id, userId }, include: { all: true } });
-        if (!post) {
-            throw new HttpException(`Post with ID ${id} not found`, HttpStatus.NOT_FOUND);
-        }
+    console.log(filterOptions)
 
-        let filename;
-        if (image) {
-            filename = await this.filesService.createFile(image);
-        }
-
-        await post.update({ ...dto, image: filename });
-
-        if (dto.value) {
-            const categories = await this.categoryRepository.findAll({ where: { value: { [Op.in]: dto.value } } });
-            if (categories.length !== dto.value.length) {
-                throw new HttpException(`One or more categories not found`, HttpStatus.NOT_FOUND);
-            }
-            await post.$set('categories', [...categories.map(category => category.id)]);
-            await post.reload();
-        }
-
-        return post;
+    if (search) {
+      filterOptions.where = { title: { [Op.iLike]: `%${search}%` } };
     }
 
-    async deletePost(id: number, userId: number,) {
-        const post = await this.postsRepository.findOne({ where: { id, userId } });
-        if (!post) {
-            throw new HttpException(`Post with ID ${id} not found`, HttpStatus.NOT_FOUND);
-        }
-        await post.destroy();
-        return "Post was deleted";
+    if (filter.length > 0) {
+      filterOptions.include[0].where = { value: { [Op.in]: filter } };
     }
+
+    return await this.postsRepository.findAll(filterOptions);
+  }
+
+  async editPost(dto: CreatePostDto, userId: number, id: number, image?: any) {
+    const post = await this.postsRepository.findOne({
+      where: { id, userId }, include: [
+        { all: true },
+        { model: Category, where: {}, attributes: ['id', 'value', 'description'] },
+        { model: User, attributes: ['id', 'login', 'email'] }
+      ],
+    });
+    if (!post) {
+      throw new HttpException(`Post with ID ${id} not found`, HttpStatus.NOT_FOUND);
+    }
+    const isTruthe = await this.isTitleExists(dto.title);
+
+    if (isTruthe) {
+      throw new BadRequestException('Title is existst');
+    }
+
+    let filename: string;
+    if (image) {
+      filename = await this.filesService.createFile(image);
+    }
+
+    await post.update({ ...dto, image: filename });
+
+    if (dto.value) {
+      const categories = await this.categoryRepository.findAll({ where: { value: { [Op.in]: dto.value } } });
+      if (categories.length !== dto.value.length) {
+        throw new HttpException(`One or more categories not found`, HttpStatus.NOT_FOUND);
+      }
+      await post.$set('categories', [...categories.map(category => category.id)]);
+      await post.reload();
+    }
+
+    return post;
+  }
+
+  async deletePost(id: number, userId: number,) {
+    const post = await this.postsRepository.findOne({ where: { id, userId } });
+    if (!post) {
+      throw new HttpException(`Post with ID ${id} not found`, HttpStatus.NOT_FOUND);
+    }
+    await post.destroy();
+    return "Post was deleted";
+  }
+
+  private async isTitleExists(title: string): Promise<boolean> {
+    const post = this.postsRepository.findOne({ where: { title } });
+    return Boolean(post);
+  }
 }
