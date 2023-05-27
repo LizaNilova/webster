53ed1698d28ad5cd9a0a5bdc2feea74eae1a3434
Herman <ushkalo.herman@gmail.com>
2023-05-27 14:22:00 +0300

    Update Post.jsx

--- webster_front/src/components/Post.jsx
+++ webster_front/src/components/Post.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { likePost } from '../redux/postsSlice';
 
 const apiPath = 'http://localhost:8080/api';
 
 const Post = ({ data, openForm }) => {
     const userID = useSelector(state => state.user.user.id)
+    const dispatch = useDispatch();
+    const [isLike, setLike] = React.useState(false)
+    const [countLikes, setCountLikes] = React.useState(data.likes.length)
+    const handleClick = (e) => {
+        try {
+            setLike(!isLike)
+            dispatch(likePost({id: data.id}))
+            if (!isLike) {
+                setCountLikes(countLikes + 1)
+            } else {
+                setCountLikes(countLikes - 1)
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    }
+    React.useEffect(() => {
+        console.log(data)
+        const isLikeUser = data.likes.find((like) => {
+            return like.userId === userID
+        })
+        if (isLikeUser) {
+            setLike(!isLike);
+        }
+    }, [])
     // console.log(userID);
     // console.log(data)
     return (
@@ -40,11 +66,16 @@ const Post = ({ data, openForm }) => {
             </div>
             {/*Likes, comments*/}
             <div className='w-full flex p-1 justify-around'>
-                <div className='p-1 mx-2 flex justify-center items-cente cursor-pointer'>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-8 h-8">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
-                    </svg>
-                    <div className='ml-1'>{data.likes.length}</div>
+                <div className='p-1 mx-2 flex justify-center items-center cursor-pointer'>
+                    {!isLike ? 
+                            <svg onClick={handleClick} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-8 h-8">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                            </svg> : 
+                            <svg onClick={handleClick} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeWidth={2} fill="currentColor" className="w-8 h-8">
+                                <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
+                            </svg>
+                    }
+                    <div className='ml-1'>{countLikes}</div>
                 </div>
                 <div className='p-1 mx-2 flex justify-center items-center cursor-pointer'>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-8 h-8">
